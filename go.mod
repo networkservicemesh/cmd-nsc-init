89cecea603d7d95e8633018b63e9dbc4b749b30c
Denis Tingaikin <denis.tingajkin@xored.com>
2021-04-06 03:43:13 +0700

    add initial version of cmd-nsc-init
    Signed-off-by: Denis Tingaikin <denis.tingajkin@xored.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
-module github.com/networkservicemesh/cmd-template
+module github.com/networkservicemesh/cmd-nsc-init
 
-go 1.15
+go 1.16
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/edwarnicke/grpcfd v0.0.0-20210219150442-10fb469a6976
+	github.com/edwarnicke/signalctx v0.0.0-20201105214533-3a35840b3011
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/networkservicemesh/api v0.0.0-20210403165646-b0b334ca6872
+	github.com/networkservicemesh/sdk v0.0.0-20210405062928-0d2dc281b97c
+	github.com/networkservicemesh/sdk-sriov v0.0.0-20210404184056-e8b6c9fd33d5
+	github.com/networkservicemesh/sdk-vpp v0.0.0-20210405063250-b9cbf6b70e98
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spiffe/go-spiffe/v2 v2.0.0-beta.5
+	google.golang.org/grpc v1.36.1
+)
---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2021 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package config contains config and helper functions for configuration cmd-nsc-init
+package config
+
+import (
+	"net/url"
+	"time"
+)
+
+// Config - configuration for cmd-nsc-init
+type Config struct {
+	Name             string        `default:"cmd-nsc-vpp" desc:"Name of Endpoint"`
+	DialTimeout      time.Duration `default:"5s" desc:"timeout to dial NSMgr" split_words:"true"`
+	RequestTimeout   time.Duration `default:"15s" desc:"timeout to request NSE" split_words:"true"`
+	ConnectTo        url.URL       `default:"unix:///var/lib/networkservicemesh/nsm.io.sock" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime time.Duration `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	NetworkServices  []url.URL     `default:"" desc:"A list of Network Service Requests" split_words:"true"`
+}
