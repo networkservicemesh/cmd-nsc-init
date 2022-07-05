e1c29434c479fdf6e76c1961c04104fc1b9f4e79
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-07-05 18:00:46 +1100

    run go mod tidy
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- internal/imports/gen.go
+++ internal/imports/gen.go
@@ -19,4 +19,4 @@
 package imports
 
 //go:generate bash -c "cd $(mktemp -d) && GO111MODULE=on go install github.com/edwarnicke/imports-gen@v1.1.1"
-//go:generate bash -c "GOOS=linux ${GOPATH}/bin/imports-gen"
+//go:generate bash -c "GOOS=linux imports-gen"
